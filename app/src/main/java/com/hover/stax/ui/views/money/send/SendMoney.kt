c111df383b51797f1aa2ee173816396770b144a1
Juma Allan <allanjuma@gmail.com>
2023-01-18 16:45:10 +0300

    working on send money flows

--- app/src/main/java/com/hover/stax/ui/views/money/send/SendMoney.kt
+++ app/src/main/java/com/hover/stax/ui/views/money/send/SendMoney.kt
@@ -9,80 +9,34 @@ import androidx.compose.material.Surface
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.Dimension
 import com.hover.stax.R
+import com.hover.stax.presentation.home.components.TopBar
 import com.hover.stax.ui.theme.StaxTheme
-import com.hover.stax.ui.views.money.data.Currency
-import com.hover.stax.ui.views.money.viewmodel.SendMoneyViewModel
-import com.hover.stax.views.compose.AmountKeyboard
-import com.hover.stax.views.compose.AmountTextField
-import com.hover.stax.views.compose.NavTopBar
-import com.hover.stax.views.compose.SendMoneyOption
-import org.koin.androidx.compose.koinViewModel
+import com.hover.stax.views.compose.NumKeypad
 
 @Composable
 fun SendMoney(
-    navTo: (dest: Int) -> Unit,
-    viewModel: SendMoneyViewModel = koinViewModel()
+    navTo: (dest: Int) -> Unit
 ) {
-    val amountText by viewModel.amountText.collectAsState()
-    val account by viewModel.account.collectAsState()
-
     StaxTheme {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
             Scaffold(
-                topBar = { NavTopBar(title = R.string.nav_send_money, navTo) }
-            ) { padding ->
-                ConstraintLayout(
-                    modifier = Modifier
-                        .padding(padding)
-                        .fillMaxSize()
-                ) {
-                    val (amountField, accountField, numKeypad, buttons) = createRefs()
+                topBar = { TopBar(title = R.string.nav_send_money, navTo) },
+                content = { padding ->
+                    NumKeypad(
+                        callback = {
 
-                    AmountTextField(modifier = Modifier.constrainAs(amountField) {
-                        top.linkTo(parent.top)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        height = Dimension.fillToConstraints
-                    }, currency = Currency.default, amount = amountText)
-
-                    SendMoneyOption(modifier = Modifier.constrainAs(accountField) {
-                        top.linkTo(amountField.bottom)
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        height = Dimension.fillToConstraints
-                    }, account = account, navTo = {})
-
-                    AmountKeyboard(
-                        modifier = Modifier.constrainAs(numKeypad) {
-                            top.linkTo(accountField.bottom)
-                            start.linkTo(parent.start)
-                            end.linkTo(parent.end)
-                            bottom.linkTo(buttons.top)
-                            height = Dimension.fillToConstraints
-                        }
-                    ) {
-                        viewModel.onKeyboardButtonClick(it)
-                    }
-
-                    Row(modifier = Modifier.constrainAs(buttons) {
-                        start.linkTo(parent.start)
-                        end.linkTo(parent.end)
-                        bottom.linkTo(parent.bottom)
-                    }) {
+                        },
+                        modifier = Modifier.padding(padding)
+                    )
+                    Row(modifier = Modifier.padding(padding)) {
                         Button(
-                            onClick = {
-
-                            }, modifier = Modifier
+                            onClick = {}, modifier = Modifier
                                 .weight(1f)
                                 .padding(padding)
                         ) {
@@ -93,9 +47,7 @@ fun SendMoney(
                             )
                         }
                         Button(
-                            onClick = {
-
-                            }, modifier = Modifier
+                            onClick = {}, modifier = Modifier
                                 .weight(1f)
                                 .padding(padding)
                         ) {
@@ -107,7 +59,7 @@ fun SendMoney(
                         }
                     }
                 }
-            }
+            )
         }
     }
 }
@@ -115,5 +67,5 @@ fun SendMoney(
 @Preview
 @Composable
 fun SendMoneyPreview() {
-//    SendMoney {}
+    SendMoney {}
 }
